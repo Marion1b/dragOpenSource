3c4e1b0a620fc3aedf904cb258090be026fa8db1
Marion1b <marion.barthoux@gmail.com>
2024-07-04 14:47:33 +0200

    test

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const contentDisplay = () => {
     fetch('contenu.json')
     .then(response => response.json())
     .then(contenu => {
-
-        const title = document.querySelector('.page-header-title');
-        title.textContent = contenu.title;
-
-        const description = document.querySelector('.page-header-par');
-        description.textContent = contenu.description;
-
-        const profilName = document.querySelector('.drag-profile');
+        const profilName = document.querySelector('.drag-profile-title');
         profilName.textContent = contenu.profile_name;
+
+        const profilePicture = document.querySelector('.drag-profile-img');
+        profilePicture.src = contenu.profile_picture;
+        profilePicture.alt = contenu.picture_description;
+
+        const instagram = document.querySelector(".media-instagram");
+        const tiktok = document.querySelector(".media-tiktok");
+        const youtube = document.querySelector(".youtube");
+        instagram.href = contenu.media_instagram;
+        tiktok.href = contenu.media_tiktok;
+        youtube.href = contenu.media_youtube;
     })
     .catch(error => console.error("Error fetching JSON data:", error));
 
