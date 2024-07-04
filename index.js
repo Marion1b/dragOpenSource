c5271b078e8fd7e2030b200949a0a28ec179eb04
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-04 14:28:15 +0200

    feat : change background color and font color in contrast, change font family

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {})
+// Fetch appearance json
+const reponse = await fetch('appearance.json')
+const appearance = await reponse.json()
+const backgroundColor = appearance['backgroundColor']
+const fontFamily = appearance['fontFamily']
+
+// Function to change font color in contrast to chosen background-color
+const getPoliceColor = (backgroundColor) => {
+  var r = parseInt(backgroundColor.substring(1, 3), 16)
+  var g = parseInt(backgroundColor.substring(3, 5), 16)
+  var b = parseInt(backgroundColor.substring(5, 7), 16)
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000
+  return yiq >= 128 ? 'black' : 'white'
+}
+
+// Fetch body and apply selected appearance
+const body = document.querySelector('body')
+body.style.backgroundColor = backgroundColor
+const color = getPoliceColor(backgroundColor)
+body.style.color = color
+body.style.fontFamily = fontFamily
