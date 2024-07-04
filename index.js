4bc0c5e247791a69ce32ab76b34f327bf1969dd7
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-04 14:47:11 +0200

    Feature/appearance json and js (#3)
    * feat : change background color and font color in contrast, change font family
    
    * refactor: code formatting

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+
+const appearanceDisplay = () => {
+  // Fetch appearance json
+  fetch('appearance.json')
+    .then((response) => response.json())
+    .then((appearance) => {
+      // Fetch body and apply selected appearance
+      const body = document.querySelector('body')
+      body.style.backgroundColor = appearance.backgroundColor
+      const color = getPoliceColor(appearance.backgroundColor)
+      body.style.color = color
+      body.style.fontFamily = appearance.fontFamily
+    })
+    .catch((error) => console.error('Error fetching JSON data:', error))
+}
+
+// Function to change font color in contrast to chosen background-color
+const getPoliceColor = (backgroundColor) => {
+  var r = parseInt(backgroundColor.substring(1, 3), 16)
+  var g = parseInt(backgroundColor.substring(3, 5), 16)
+  var b = parseInt(backgroundColor.substring(5, 7), 16)
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000
+  return yiq >= 128 ? 'black' : 'white'
+}
+
+
 const contentDisplay = () => {
 
     fetch('contenu.json')
@@ -15,21 +41,10 @@ const contentDisplay = () => {
     })
     .catch(error => console.error("Error fetching JSON data:", error));
 
-
-
 }
 
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-    contentDisplay();
-
-
-
-
-
+  contentDisplay();
+  appearanceDisplay()
 
 });
