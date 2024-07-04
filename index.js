428926a230d2c684b7417be940eacfa9a3f3727e
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-07-04 16:00:02 +0200

    Feature/links and borders recolor (#6)
    * feat: link recolor
    
    * feat: border recolor

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const appearanceDisplay = () => {
   // Fetch appearance json
   fetch('appearance.json')
@@ -10,6 +9,14 @@ const appearanceDisplay = () => {
       const color = getPoliceColor(appearance.backgroundColor)
       body.style.color = color
       body.style.fontFamily = appearance.fontFamily
+      const links = document.querySelectorAll('a')
+      links.forEach((link) => {
+        link.style.color = color
+      })
+      const sections = document.querySelectorAll('section')
+      sections.forEach((section) => {
+        section.style.border = `${color} .2rem solid`
+      })
     })
     .catch((error) => console.error('Error fetching JSON data:', error))
 }
@@ -23,46 +30,44 @@ const getPoliceColor = (backgroundColor) => {
   return yiq >= 128 ? 'black' : 'white'
 }
 
-
 const contentDisplay = () => {
+  fetch('contenu.json')
+    .then((response) => response.json())
+    .then((contenu) => {
+      const profilName = document.querySelector('.drag-profile-title')
+      profilName.textContent = contenu.profile_name
 
-    fetch('contenu.json')
-    .then(response => response.json())
-    .then(contenu => {
-        const profilName = document.querySelector('.drag-profile-title');
-        profilName.textContent = contenu.profile_name;
+      const profilePicture = document.querySelector('.drag-profile-img')
+      profilePicture.src = contenu.profile_picture
+      profilePicture.alt = contenu.picture_description
 
-        const profilePicture = document.querySelector('.drag-profile-img');
-        profilePicture.src = contenu.profile_picture;
-        profilePicture.alt = contenu.picture_description;
+      const instagram = document.querySelector('.media-instagram')
+      const tiktok = document.querySelector('.media-tiktok')
+      const youtube = document.querySelector('.media-youtube')
+      instagram.href = contenu.media_instagram
+      tiktok.href = contenu.media_tiktok
+      youtube.href = contenu.media_youtube
 
-        const instagram = document.querySelector(".media-instagram");
-        const tiktok = document.querySelector(".media-tiktok");
-        const youtube = document.querySelector(".media-youtube");
-        instagram.href = contenu.media_instagram;
-        tiktok.href = contenu.media_tiktok;
-        youtube.href = contenu.media_youtube;
+      const linkContact = document.querySelector('.contact-link')
+      linkContact.href = 'mailto:' + contenu.email_contact
+      linkContact.innerText = contenu.email_contact
 
-        const linkContact = document.querySelector(".contact-link");
-        linkContact.href = "mailto:" + contenu.email_contact;
-        linkContact.innerText = contenu.email_contact;
+      const familyName = document.querySelector('.family-name')
+      familyName.innerText = contenu.dragFamily_name
 
-        const familyName = document.querySelector(".family-name");
-        familyName.innerText = contenu.dragFamily_name;
+      const familyInstagram = document.querySelector('.family-instagram')
+      const familyTiktok = document.querySelector('.family-tiktok')
+      const familyYoutube = document.querySelector('.family-youtube')
+      familyInstagram.href = contenu.family_instagram
+      familyTiktok.href = contenu.family_tiktok
+      familyYoutube.href = contenu.family_youtube
 
-        const familyInstagram = document.querySelector(".family-instagram");
-        const familyTiktok = document.querySelector(".family-tiktok");
-        const familyYoutube = document.querySelector(".family-youtube");
-        familyInstagram.href = contenu.family_instagram;
-        familyTiktok.href = contenu.family_tiktok;
-        familyYoutube.href=contenu.family_youtube;
-
-        const regularEvents = document.querySelector(".regular-list");
-        for(let key in contenu.regular_events){
-            const li = document.createElement('li');
-            li.innerText = contenu.regular_events[key];
-            regularEvents.appendChild(li);
-        }
+      const regularEvents = document.querySelector('.regular-list')
+      for (let key in contenu.regular_events) {
+        const li = document.createElement('li')
+        li.innerText = contenu.regular_events[key]
+        regularEvents.appendChild(li)
+      }
 
         const exceptionnalEvents = document.querySelector(".exceptional-list");
         for(let key in contenu.exceptional_events){
@@ -74,12 +79,10 @@ const contentDisplay = () => {
         const TalentContainer = document.querySelector(".talent-container");
         TalentContainer.innerHTML = contenu.talent;
     })
-    .catch(error => console.error("Error fetching JSON data:", error));
-
+    .catch((error) => console.error('Error fetching JSON data:', error))
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  contentDisplay();
+  contentDisplay()
   appearanceDisplay()
-
-});
+})
