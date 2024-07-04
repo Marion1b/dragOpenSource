f2e7df4c9cea2f466ad0fd33618d5bac582ccc86
benjaminhaverbeke <banjamin.haverbeke@outlook.com>
2024-07-04 13:54:31 +0200

    contenu json et dynamisation

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {})
+const contentDisplay = () => {
+
+    fetch('contenu.json')
+    .then(response => response.json())
+    .then(contenu => {
+
+        const title = document.querySelector('.page-header-title');
+        title.textContent = contenu.title;
+
+        const description = document.querySelector('.page-header-par');
+        description.textContent = contenu.description;
+
+        const profilName = document.querySelector('.drag-profile');
+        profilName.textContent = contenu.profile_name;
+    })
+    .catch(error => console.error("Error fetching JSON data:", error));
+
+
+
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    contentDisplay();
+
+
+
+
+
+
+});
